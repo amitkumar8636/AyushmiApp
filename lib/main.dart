a369e60f3c31f260a0f4e9653bd821c3c6bc92a3
Amit Kumar <amitkumar8636@gmail.com>
2021-07-17 14:01:27 +0530

    Initial Working Model for the app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:zerodhaui/ui/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  //  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
